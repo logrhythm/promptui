9590a952a369aaabf3f26be2dbb5144b6e5db8cf
Taylor Andrews <taylor.andrews@logrhythm.com>
2019-11-11 11:20:11 -0700

    Remove gometalinter

--- tools.go
+++ tools.go
@@ -1,11 +1,3 @@
 // +build tools
 
 package tools
-
-import (
-	"github.com/alecthomas/gometalinter"
-	"github.com/client9/misspell/cmd/misspell" // lint
-	"github.com/gordonklaus/ineffassign"       // lint
-	"github.com/tsenart/deadcode"              // lint
-	"golang.org/x/lint/golint"                 // lint
-)
